c34a1da38ab12d58aaa3a6811a1d18696c240b68
Laila Campbell <lailacampbell96@gmail.com>
2020-02-12 03:58:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,5 @@
 # lib/introduction.rb
-
+ 
 def introduction(name)
   puts "Hi, my name is #{name}."
-  # lib/introduction.rb
-
-def introduction (name and language)
-  puts Hi, my name is #{name} and I am learning to program in #{language}."
 end
